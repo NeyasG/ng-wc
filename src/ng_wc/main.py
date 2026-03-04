675c02f4fa4012afb205d1b919812a443f5a712d
= <=>
2026-03-04 23:43:12 +0000

    feat: Add second flat to count lines

--- src/ng_wc/main.py
+++ src/ng_wc/main.py
@@ -5,27 +5,48 @@ import os
 from pathlib import Path
 
 
-def main(*args, **kwargs) -> None:
+def main(argv: list[str] | None = None) -> None:
     """Count words in a file.
 
     Parses and displays the word count from a specified file using argparse.
     Supports optional count filtering for specific word patterns.
 
     Args:
-        *args: Variable length argument list.
-        **kwargs: Arbitrary keyword arguments.
+        argv: Optional list of arguments
 
     Returns:
         None
     """
     parser = argparse.ArgumentParser(prog="NG Word Count", description="Parses word count in files")
-    parser.add_argument("-c", "--count", help="Count the number of bytes in a file.")
+    parser.add_argument("-c", "--count", help="Count the number of bytes in a file.", action="store_true")
+    parser.add_argument("-l", "--lines", help="Outputs the number of lines in a file", action="store_true")
+    parser.add_argument("file_path", nargs="?", help="Path to input file")
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
-    file_path = Path(args.count)
+    if args.file_path is None and not (args.count or args.lines):
+        parser.print_help()
+        return
 
-    print(f"{os.path.getsize(file_path)} {file_path.name}")
+    if args.file_path is None:
+        parser.error("the following arguments are required: file_path")
+
+    if not args.count and not args.lines:
+        parser.print_help()
+        return
+
+    file_path = Path(args.file_path)
+
+    if not file_path.is_file():
+        print(f"The file {file_path!s} is not a valid file, or does not exist.")
+        raise SystemExit(1)
+
+    if args.count:
+        print(f"{os.path.getsize(file_path)} {file_path.name}")
+
+    if args.lines:
+        with open(file_path) as f:
+            print(f"{sum(1 for _ in f)} {file_path.name}")
 
 
 if __name__ == "__main__":  # pragma: no cover
--- tests/test_example.py
+++ tests/test_example.py
@@ -2,10 +2,18 @@
 
 import subprocess
 
+TESTFILE_PATH = "tests/fixtures/test.txt"
+
 
 class TestSteps:
     def test_step_one(self):
-        result = subprocess.run(["ngwc", "-c", "tests/fixtures/test.txt"], capture_output=True, text=True)  # noqa: S607
+        result = subprocess.run(["ngwc", "-c", TESTFILE_PATH], capture_output=True, text=True)  # noqa: S603, S607
 
         assert result.stdout.strip() == "342143 test.txt"
+        assert result.returncode == 0
+
+    def test_step_two(self):
+        result = subprocess.run(["ngwc", "-l", TESTFILE_PATH], capture_output=True, text=True)  # noqa: S603, S607
+
+        assert result.stdout.strip() == "7143 test.txt"
         assert result.returncode == 0
